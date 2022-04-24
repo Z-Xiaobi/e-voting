a23dfb26f296803d181ead5c9d312236a7bddd2a
Z-Xiaobi <935213752@qq.com>
2022-04-24 17:42:11 -0400

    Update BlockChain class with PoW and adding functions

--- BlockChain.py
+++ BlockChain.py
@@ -37,9 +37,13 @@ class Block:
 
 
 class BlockChain:
+
+    difficulty = 3
+
     def __init__(self):
         self.block_chain = []
         self.create_initial_block()
+        self.unconfirmed_transactions = []
 
     def create_initial_block(self):
         """
@@ -50,9 +54,78 @@ class BlockChain:
         initial_block.block_hash = initial_block.generate_hash()
         self.block_chain.append(initial_block)
 
+    @property
     def get_last_block(self):
         """
         Retrieve the most recent block in the block chain.
         Note that The block chain will always have at least one block.
         """
         return self.block_chain[-1]
+
+    def proof_of_work(self, block):
+        """
+        Avoid hash update with old prev_block_hash value.
+        Exploit asymmetry Function that tries different values of
+        the nonce (editable dummy data) to get a hash
+        that satisfies difficulty criteria.
+        """
+        block.nonce = 0
+
+        generated_hash = block.generate_hash()
+
+        while not generated_hash.startswith('0' * self.difficulty):
+            block.nonce += 1
+            generated_hash = block.generate_hash()
+
+        return generated_hash
+
+    def is_valid_proof(self, block, block_hash):
+        """
+        Check if block_hash is valid and satisfies the difficulty criteria.
+        """
+        return (block_hash.startswith('0' * self.difficulty) and
+                block_hash == block.generate_hash())
+
+    def add_block(self, block, proof):
+        """
+        Verify the data (proof of work)
+        Verify order of transactions is preserved (prev_block_hash field)
+        :param block: a created block
+        :param proof: hash
+        :return:
+        """
+        prev_block_hash = self.get_last_block.hash
+
+        if prev_block_hash != block.prev_block_hash:
+            return False
+
+        if not self.is_valid_proof(block, proof):
+            return False
+
+        block.hash = proof
+        self.block_chain.append(block)
+        return True
+
+    def add_new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
+
+    def mine(self):
+        """
+        The transactions will be initially stored as a pool of unconfirmed transactions.
+        Mining process: Add the pending transactions to the blockchain
+        by adding them to the block and calculate Proof Of Work.
+        """
+        # if no unconfirmed transactions
+        if not self.unconfirmed_transactions:
+            return False
+
+        new_block = Block(index=self.get_last_block.index + 1,
+                          timestamp=time.time(),
+                          prev_block_hash=self.get_last_block.hash,
+                          transaction_list=self.unconfirmed_transactions,)
+
+        proof = self.proof_of_work(new_block)
+        self.add_block(new_block, proof)
+
+        self.unconfirmed_transactions = []
+        return new_block.index
